f8a44cd1863d05929b61c3a9c836b770094422f5
smokysk <samuelviana2626@gmail.com>
2019-11-25 21:05:13 -0400

    condicional

---
+++ ExercisesCondicional/3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers");
+            string[] hours = Console.ReadLine().Split(' ');
+            int begin = int.Parse(hours[0]);
+            int finish = int.Parse(hours[1]);
+
+            int duration;
+            if(begin < finish){
+                duration = finish - begin;
+            }
+            else{
+                duration = 24 - begin + finish;
+            }
+
+            System.Console.WriteLine("A duraçao do jogo foi "+ duration );
+        }
+    }
+}
